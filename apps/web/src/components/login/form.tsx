94aab25508fc10adce862fe51e7846fb913260be
Mario Pon <mariobpon11@gmail.com>
2025-12-14 10:43:43 -0600

    feat: enhance login form with error handling and remember me functionality

--- apps/web/src/components/login/form.tsx
+++ apps/web/src/components/login/form.tsx
@@ -2,11 +2,17 @@
 
 import type React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Mail } from "lucide-react";
+import { Info, Mail } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
 
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@portfolio/ui/components/alert";
 import { Button } from "@portfolio/ui/components/button";
 import {
   Card,
@@ -28,31 +34,58 @@ import { PasswordInput } from "@portfolio/ui/components/password-input";
 import { Separator } from "@portfolio/ui/components/separator";
 import { cn } from "@portfolio/ui/lib/utils";
 
+import { signIn } from "@/hooks/auth";
+
 import { GithubLogin } from "./github";
 
 type LoginFormProps = React.ComponentProps<typeof Card>;
 
 const formSchema = z.object({
   email: z.email(),
-  password: z.string().min(8),
-  remember: z.boolean().optional(),
+  password: z
+    .string()
+    .min(8, { error: "Password must be at least 8 characters long" }),
+  rememberMe: z.boolean().optional(),
 });
 
 type FormData = z.infer<typeof formSchema>;
 
 export function LoginCard({ className, ...props }: LoginFormProps) {
+  const router = useRouter();
   const form = useForm<FormData>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
       password: "",
-      remember: false,
+      rememberMe: false,
     },
     mode: "onSubmit",
   });
 
   async function onSubmit(data: FormData) {
-    console.log(data);
+    await signIn.email(
+      {
+        ...data,
+      },
+      {
+        onError: (ctx) => {
+          if (ctx.error.status === 401) {
+            form.setError("email", { message: "Invalid email or password" });
+            form.setError("password", { message: "Invalid email or password" });
+          } else {
+            console.error(ctx.error);
+            form.setError("root", {
+              message: "An error occurred while signing in",
+            });
+          }
+        },
+        onSuccess: (ctx) => {
+          if (ctx.data.twoFactorRedirect) {
+            router.push("/verify");
+          }
+        },
+      },
+    );
   }
 
   return (
@@ -64,6 +97,15 @@ export function LoginCard({ className, ...props }: LoginFormProps) {
         </CardDescription>
       </CardHeader>
       <CardContent className="space-y-8">
+        {form.formState.errors.root && (
+          <Alert variant="destructive">
+            <Info className="size-4" />
+            <AlertTitle>Error</AlertTitle>
+            <AlertDescription className="text-muted-foreground!">
+              {form.formState.errors.root.message}
+            </AlertDescription>
+          </Alert>
+        )}
         <form
           onSubmit={form.handleSubmit(onSubmit)}
           id="sign-in"
@@ -110,7 +152,7 @@ export function LoginCard({ className, ...props }: LoginFormProps) {
           />
           <Controller
             control={form.control}
-            name="remember"
+            name="rememberMe"
             render={({ field }) => (
               <div className="mt-2 flex w-full flex-row items-center justify-between">
                 <Field className="flex flex-1 flex-row items-center gap-2">
@@ -118,10 +160,10 @@ export function LoginCard({ className, ...props }: LoginFormProps) {
                     <Checkbox
                       checked={field.value}
                       onCheckedChange={field.onChange}
-                      id="remember"
+                      id="rememberMe"
                     />
                   </FieldContent>
-                  <FieldLabel htmlFor="remember">Remember me</FieldLabel>
+                  <FieldLabel htmlFor="rememberMe">Remember me</FieldLabel>
                 </Field>
                 <Link
                   href="/forgot-password"
