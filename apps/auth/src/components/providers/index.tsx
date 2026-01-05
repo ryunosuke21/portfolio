1035081dc29ff003814eb80953d7d3da921544fa
Mario Pon <mariobpon11@gmail.com>
2026-01-04 22:32:59 -0600

    fix: remove rich colors

--- apps/auth/src/components/providers/index.tsx
+++ apps/auth/src/components/providers/index.tsx
@@ -17,7 +17,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       >
         {children}
         <Analytics />
-        <Toaster richColors />
+        <Toaster />
       </ThemeProvider>
     </TRPCReactProvider>
   );
